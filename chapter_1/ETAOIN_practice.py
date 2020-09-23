9b80d6631549ab06a0bcbeac607a6ec2a632b54e
Gimpei Misono <ginpeimisonou@gmail.com>
2020-09-24 08:25:40 +0900

    Add ETAOIN

---
+++ chapter_1/ETAOIN_practice.py
@@ -0,0 +1,20 @@
+"""Map letters from string into dictionary & print bar chart of frequency."""
+import pprint
+from collections import defaultdict
+
+# Note: text should be a short phrase for bars to fit in IDLE window
+text = 'Like the castle in its corner in a medieval game, I foresee terrible \
+trouble and I stay here just the same.'
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+mapped = defaultdict(list)
+for character in text:
+    character = character.lower()
+    if character in ALPHABET:
+        mapped[character].append(character)
+
+# pprint lets you print stacked output
+print("\nYou may need to stretch console window if text wrapping occurs.\n")
+print("text = {}".format(text))
+pprint.pprint(mapped, width=110)
